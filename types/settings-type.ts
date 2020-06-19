b2242e0d017505cf3092a6fdb5cb129f3161b2e2
hjcosta <hjcosta.dev@gmail.com>
2020-06-19 02:17:07 -0300

    commit inicial

---
+++ types/settings-type.ts
@@ -0,0 +1,32 @@
+import { ConfigTP } from "./config-types"
+
+/**
+ * TODO: Add desc
+ */
+
+export type SettingsTP = {
+    general?: GeneralSettingsTP,
+    specific?: ConfigTP[],
+}
+
+export type VarsTP = Required<
+    & GeneralForArrayTP
+    & Record<keyof GeneralForRegexTP, string>
+    & { arraySuffixes: string, booleanPrefixes: string }
+>
+
+export type GeneralSettingsTP = Record<keyof GeneralForRegexTP | keyof GeneralForArrayTP, string[]>
+
+type GeneralForRegexTP = {
+    functionPrefixes?: string[],
+    booleanPrefixesLC?: string[],
+    booleanPrefixesUC?: string[],
+    arraySuffixesLC?: string[],
+    arraySuffixesUC?: string[],
+}
+
+type GeneralForArrayTP = {
+    classSuffixes?: string[],
+    typeSuffixes?: string[],
+    typeSuffixesGenerics?: string[],
+}
